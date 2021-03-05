e4c91c61167d471b5d264e3546c8f62014df9b7c
akhena1 <gu.marcondes2012@gmail.com>
2021-03-04 22:42:09 -0300

    initial config

---
+++ drinkhappy/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View><Text>Estrutura inicial</Text></View>
+    </>
+  );
+};
+
+export default App;
